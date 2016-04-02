22ad38f15b8b60dee99106fb4156264f2147b7fb
Steve DIAS <steveamado.dias@gmail.com>
2016-04-02 16:10:34 +0200

    Suppression de l'attribue type de ses getter setter, modification du constructeur.

--- GLP-BOURSE/src/data/MarcheAction.java
+++ GLP-BOURSE/src/data/MarcheAction.java
@@ -2,22 +2,16 @@ package data;
 
 public class MarcheAction extends Marche{
 
-	private String type;
+	
 	
 	
 	public MarcheAction(String type,int idMarche, int idEntreprise, double prixCourant, double prixBas, double prixHaut) {
 		super(idMarche, idEntreprise, prixCourant, prixBas, prixHaut);
-		this.type=type;
+		super.setType(type);
 	}
 
 	
 	
 	
-	public String getType() {
-		return type;
-	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
 }
